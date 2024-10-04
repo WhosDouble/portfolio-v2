27aa4e84c6273c9e0471a0c9e61817d6dc45e4cf
SirRango <sturdivantdevon78@gmail.com>
2024-10-04 13:02:27 -0500

    big updates (hopefully it still works)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { AnimatedBackground } from "animated-backgrounds";
+import "./App.css";
+import Loading from "./componets/Loader";
+import Navbar from "./componets/Navbar";
+import MainTitle from "./componets/MainTitle";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //useState() for the loader
+  const [loading, setLoading] = useState(true);
 
+  //useEffect to add a delay before showing content
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div>
+          <AnimatedBackground animationName="auroraBorealis" />
+          <Navbar />
+          <MainTitle />
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
